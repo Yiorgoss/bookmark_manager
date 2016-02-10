00bb4d9a846bd3f20acc4fb1ad5a61949b989367
yiorgoss <makridakisyiorgos@gmail.com>
2016-02-10 19:20:51 +0000

    code done however database still needs link and shit

--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -5,3 +5,11 @@ def add_full_entry
   fill_in 'tag', with: 'CCC'
   click_button 'Submit'
 end
+
+def sign_up
+  visit '/users/new'
+  expect(page.status_code).to eq(200)
+  fill_in :email, with: 'example@example.com'
+  fill_in :password, with: 'example'
+  click_button 'Sign_up'
+end
